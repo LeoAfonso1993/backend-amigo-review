f59617e6dd22cb902b2e689841076dc35b1dc5cb
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2023-02-28 21:54:46 -0500

    Login with devise implemented

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+ root "home#index"
+
+end
